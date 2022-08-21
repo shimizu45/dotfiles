ec2102c12a78071824fa00755e76168a55d6d0d7
shimizu45 <shimizu.renk@gmail.com>
2022-08-22 00:49:16 +0900

    added linux/ and modified set.sh

--- .bin/set.sh
+++ .bin/set.sh
@@ -15,46 +15,44 @@ link_to_homedir() {
 
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
   local dotdir=$(dirname ${script_dir})
+  case $OSTYPE in
+      "linux-gnu"*)
+          local osdir="linux"
+          ;;
+      "darwin"*)
+          local osdir="darwin"
+          ;;
+  esac
+
   if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-        [[ `basename $f` == ".git" || `basename $f` == ".bin"* ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
-      fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
-      fi
-      command ln -snf $f $HOME
-    done
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        for f in $dotdir/linux/.??*; do
-            if [[ -L "$HOME/`basename $f`" ]];then
-                command rm -f "$HOME/`basename $f`"
-            fi
-            if [[ -e "$HOME/`basename $f`" ]];then
-                command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
-            fi
-            command ln -snf $f $HOME
-        done
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        for f in $dotdir/darwin/.??*; do
-            if [[ -L "$HOME/`basename $f`" ]];then
-                command rm -f "$HOME/`basename $f`"
-            fi
-            if [[ -e "$HOME/`basename $f`" ]];then
-                command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
-            fi
-            command ln -snf $f $HOME
-        done
-    fi
-else
-    command echo "same install src dest"
+      for f in $dotdir/.??*; do
+          [[ `basename $f` == ".git" || `basename $f` == ".bin" ]] && continue
+          if [[ -L "$HOME/`basename $f`" ]];then
+              command rm -f "$HOME/`basename $f`"
+          fi
+          if [[ -e "$HOME/`basename $f`" ]];then
+              command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+          fi
+          command ln -snf $f $HOME
+      done
+      for f in `find $dotdir/$osdir/ -type f`; do
+          if [[ -L "$HOME${f#$dotdir/$osdir}" ]];then
+              command rm -f "$HOME${f#$dotdir/$osdir}"
+          elif [[ -e "$HOME${f#$dotdir/$osdir}" ]];then
+              command mkdir -p "$HOME/.dotbackup$(dirname ${f#$dotdir/$osdir})"
+              command mv "$HOME${f#$dotdir/$osdir}" "$HOME/.dotbackup${f#$dotdir/$osdir}"
+          fi
+          command mkdir -p "$HOME$(dirname ${f#$dotdir/$osdir})"
+          command ln -snf $f $(dirname $HOME${f#$dotdir/$osdir})
+      done
+  else
+      command echo "same install src dest"
   fi
 }
 
 while [ $# -gt 0 ];do
-  case ${1} in
-    --debug|-d)
+    case ${1} in
+        --debug|-d)
       set -uex
       ;;
     --help|-h)
